2e9d9ff27c94be941adc2ce78314042455b9fd65
alyokhina-o <alyokhina-o@yandex-team.ru>
2019-05-27 18:20:59 +0300

    added creation dsl context

--- server/domain/src/main/java/ru/hse/spb/keylogger/UserRepository.java
+++ server/domain/src/main/java/ru/hse/spb/keylogger/UserRepository.java
@@ -1,15 +1,34 @@
 package ru.hse.spb.keylogger;
 
 import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.jooq.impl.DefaultDSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import ru.hse.spb.keylogger.db.tables.Worker;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @Repository
 public class UserRepository {
-    private final DSLContext create;
+    String user = "keylogger_user";
+    String pass = "password";
+    String url = "jdbc:postgresql://localhost:5432/keylogger";
 
-    @Autowired
-    public UserRepository(DSLContext dslContext) {
-        this.create = dslContext;
+    public UserRepository() {
+        try (Connection conn = DriverManager.getConnection(url, user, pass)) {
+            // Create a context for your database
+            DSLContext ctx = DSL.using(conn, SQLDialect.POSTGRES_9_4);
+            ctx.insertInto(Worker.WORKER)
+                    .columns(Worker.WORKER.LOGIN, Worker.WORKER.PASSWORD, Worker.WORKER.TOKEN)
+                    .values("a", "b", "c")
+                    .execute();
+            // Do something useful ...
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
